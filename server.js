6e14b0793dcfe79cb8de8dd1691593f894ec25e7
Abiola-Farounbi <farounbiabiola@gmail.com>
2023-02-15 08:41:08 +0100

    Added comments to the code

--- server.js
+++ server.js
@@ -1,43 +1,46 @@
 "use strict";
 
-
-
+// Import required modules
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 
+// Create an Express app instance
 const app = express();
+
+// Create an HTTP server using the Express app instance
 const server = http.createServer(app);
+
+// Create a WebSocket server instance and attach it to the HTTP server
 const websocketServer = new WebSocket.Server({ server });
 
-//start the web server
+// Start the server listening on port 3000
 server.listen(3000, () => {
   console.log("Websocket server started on port 3000");
 });
 
-
-// when a websocket connection is established
-// Event handler for when a client connects to the server
+// Listen for WebSocket connections
 websocketServer.on('connection', (socket) => {
+
+  // Log a message when a new client connects
   console.log('client connected.');
 
-
-  // Event handler for when a message is received from the client
+  // Listen for incoming WebSocket messages
   socket.on('message', (data) => {
-    // Broadcast the message to all connected clients
+   
+   // Broadcast the message to all connected clients
     websocketServer.clients.forEach(function each(client) {
       if (client !== socket && client.readyState === WebSocket.OPEN) {
         client.send(data.toString());
-        console.log("message",data.toString())
+        // console.log("message",data.toString())
       }
     });
   });
 
-  // Event handler for when a client disconnects from the server
+  // Listen for WebSocket connection close events
   socket.on('close', () => {
+    // Log a message when a client disconnects
     console.log('Client disconnected');
   });
 
 });
-
-
